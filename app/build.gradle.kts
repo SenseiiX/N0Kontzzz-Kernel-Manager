c4a81798e67751e03f1f61e2d6e672e8cf35e940
bimoalfarrabi <bimoalfarrabi24@gmail.com>
2025-12-18 11:22:23 +0700

    NKM: release 1.3.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "id.nkz.nokontzzzmanager"
         minSdk = 26
         targetSdk = 36
-        versionCode = 46
-        versionName = "1.3.1"
+        versionCode = 48
+        versionName = "1.3.2"
     }
     buildTypes {
         release {
